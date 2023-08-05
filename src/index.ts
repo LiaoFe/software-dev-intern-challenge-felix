93522cea32ba03bb2048602b9d672165de7fe3c4
felix <f4liao@uwaterloo.ca>
2023-08-05 17:13:26 -0400

    final submission

--- src/index.ts
+++ src/index.ts
@@ -27,10 +27,63 @@ interface Frame {
   y3: number;
 }
 
+const THRESHOLD_CLOSED = Math.PI / 36; // 5 degrees
+const THRESHOLD_OPEN = Math.PI / 4; // 45 degrees
+
+// Helper function: to determine the distance of 2 points
+// params:
+// x1, y1, x2, y2: number inputs where (x1, y1) is a coordinates of 1 point and (x2, y2) is the coordinate
+//                  of a second point
+// return: non-negative integer distance of point (x1, y1) and (x2, y2)
+const dist = (x1: number, y1: number, x2: number, y2: number): number => {
+  // console.log(`**** dist1: ${(x1 - x2) ** 2} ****`);
+  // console.log(`**** dist1: ${(y1 - y2) ** 2} ****`);
+  return Math.sqrt((x1 - x2) ** 2 + (y1 - y2) ** 2);
+};
+
+// Helper function: given triangle with side A, B, C, return the angle of ABC
+//                  (returns the angle between side A, and B, with C as the side opposite to the interested angle)
+// params:
+// lenA, lenB, lenC: number inputs where lenA, lenB, lenC are the side lengths of the traingle
+//                    of a second point
+// return: non-negative integer representing the angle of ABC in **RADIANS**, the angle between side A and B,
+//         with C as the opposite side length of the interested angle
+const cosineLaw = (lenA: number, lenB: number, lenC: number): number => {
+  return Math.acos((lenA ** 2 + lenB ** 2 - lenC ** 2) / (2 * lenA * lenB));
+};
+
 const calculateOpenings = (frames: Frame[]): number => {
-  const openings = 0;
+  var openings = 0;
+  var closings = 0;
 
   // Write your algorithm
+
+  // determine start position (open or closed)?
+  var isOpen: boolean = false;
+  var firstIt: boolean = true;
+
+  // iterate through frames array
+  frames.forEach((frame) => {
+    // initalize lengths, A side from center to top point, B wil be from center to bottom, C will be from top to bottom
+    var lenA = dist(frame.x1, frame.y1, frame.x2, frame.y2);
+    var lenB = dist(frame.x1, frame.y1, frame.x3, frame.y3);
+    var lenC = dist(frame.x2, frame.y2, frame.x3, frame.y3);
+
+    // determine angle in radians
+    var angle = cosineLaw(lenA, lenB, lenC);
+
+    if (firstIt) {
+      if (angle <= THRESHOLD_CLOSED) isOpen = false;
+      if (angle >= THRESHOLD_OPEN) isOpen = true;
+      firstIt = false;
+    } else if (isOpen && angle <= THRESHOLD_CLOSED) {
+      isOpen = false;
+      closings++;
+    } else if (!isOpen && angle >= THRESHOLD_OPEN) {
+      isOpen = true;
+      openings++;
+    }
+  });
 
   console.log(`**** Openings: ${openings} ****`);
   return openings;
